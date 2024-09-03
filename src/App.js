216c3250c299e6043577045360a4bb397a30ba6e
Maribel <maribelsoledadalarcon@gmail.com>
2024-09-03 17:54:44 +0200

    first commit - initialize project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React from "react";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <nav className="navbar">
+        <div className="navbar-logo">
+          {/*  */}
+          <img src="/path-to-logo.png" alt="Webflow Community Logo" />
+        </div>
+
+        <ul className="navbar-links">
+          <li>
+            <a href="#">Watch & Listen</a>
+          </li>
+          <li>
+            <a href="#">Events</a>
+          </li>
+          <li>
+            <a href="#">Essentials</a>
+          </li>
+          <li>
+            <a href="#">More</a>
+          </li>
+        </ul>
+
+        <div className="navbar-actions">
+          <button className="btn-get-started">Login</button>
+        </div>
+      </nav>
+
+      <div className="app-containerer">
+        <header className="header">
+          <div className="header-content">
+            <h1>Connect and build together</h1>
+            <p>
+              {" "}
+              Connect and build with 85,000+ community members around the world
+              — whether you're new to Webflow or a seasoned designer, there's a
+              place for you.
+            </p>
+          </div>
+
+          <div className="header-image">
+            <img src="earth.png" />
+          </div>
+        </header>
+        <section className="livestreams-section"></section>
+      </div>
+    </>
   );
 }
 
